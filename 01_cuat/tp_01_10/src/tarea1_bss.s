7d3e119c89410084911a6230a36cc7d724064326
Juan Alberto Arean <juan.arean@gmail.com>
2018-06-16 13:41:58 -0300

    correccion tp11

--- 01_cuat/tp_01_10/src/tarea1_bss.s
+++
@@ -1,8 +0,0 @@
-SECTION	.tarea1_bss nobits alloc noexec write
-
-GLOBAL _sumatoria
-
-
-_sumatoria:
-    resb 4
-    
--- 01_cuat/tp_01_10/src/tarea1_data.s
+++
@@ -1,7 +0,0 @@
-SECTION	.tarea1_datos nobits alloc noexec write
-
-GLOBAL _DATO1
-
-
-_DATO1: db  0x01
-    
